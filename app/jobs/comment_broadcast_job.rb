5227756e30b7d836913a923909afd3154fac44da
Dan Uluma <danuluma@gmail.com>
2018-08-03 21:19:39 +0300

    created commentbroadcast

---
+++ app/jobs/comment_broadcast_job.rb
@@ -0,0 +1,16 @@
+class CommentBroadcastJob < ApplicationJob
+
+ queue_as :default
+ 
+ def perfom(comment)
+  ActionCable.server.broadcast "blogs_#{comment.blog.id}_channel", comment: render_comment(comment)
+
+end
+
+def render_comment(comment)
+  CommentsController.render partial: "comments/comment", locals: {
+    comment: comment
+  }
+end
+
+end
