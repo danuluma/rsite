f913b81397652f1c0c959de043b2219dea7e8de6
Dan Uluma <danuluma@gmail.com>
2018-08-08 21:15:53 +0300

    added undo and redo capability to blogs

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -37,13 +37,9 @@ class BlogsController < ApplicationController
     # byebug 
     respond_to do |format|
       if @blog.save
-        format.html { redirect_to @blog, notice: 'Blog created, lala sasa' }
-
-       
-        
+        format.html { redirect_to @blog, notice: "Blog created, lala sasa #{undo_link}" }
       else
-        format.html { render :new }
-        
+        format.html { render :new }  
       end
     end
   end
@@ -60,7 +56,7 @@ class BlogsController < ApplicationController
 
         # undo_link = view_context.link_to("undo", revert_version_path(@product.versions.last), :method => :post)
 
-        format.html { redirect_to @blog, notice: "Post was updated! #{make_undo_link}" }
+        format.html { redirect_to @blog, notice: "Post was updated! #{undo_link}" }
 
         # format.html { redirect_to @blog, notice: 'Blog updated pewa mbili ' }
         
@@ -76,7 +72,7 @@ class BlogsController < ApplicationController
   def destroy
     @blog.destroy
     respond_to do |format|
-      format.html { redirect_to blogs_url, notice: 'Blog was successfully destroyed.' }
+      format.html { redirect_to blogs_url, notice: "Blog was successfully destroyed. #{undo_link}"}
       format.json { head :no_content }
     end
   end
@@ -84,44 +80,31 @@ class BlogsController < ApplicationController
   def toggle_status
     if @blog.published? 
      @blog.draft!
-    elsif @blog.draft? 
+   elsif @blog.draft? 
      @blog.published!
-    end
+   end
 
-    redirect_to blogs_url
+   redirect_to blogs_url
   end
 
  
  # papertrail
 
   def history
-  @versions = PaperTrail::Version.order('created_at DESC')
+    @blog = Blog.find(params[:id])
+    @versions = @blog.try(:versions).order(created_at: :desc) unless
+    @blog.blank? || @blog.versions.blank?
   end
 
-  def undo
-    @blog_version = PaperTrail::Version.find_by_id(params[:id])
-
-    begin
-      if @blog_version.reify
-        @blog_version.reify.save
-      else
-        # For undoing the create action
-        @blog_version.item.destroy
-      end
-      flash[:success] = "Undid that!"
-    rescue
-      flash[:alert] = "Failed undoing the action..."
-    ensure
-      redirect_to root_path
-    end
-  end
 
   private
 
+  def undo_link
+    view_context.link_to("undo", revert_version_path(@blog.versions.last), method: :post)
+  end
+
   def make_undo_link
-   
     view_context.link_to 'Undo that plz!', undo_path(@blog.versions.last), method: :post
-    
   end
 
   # Use callbacks to share common setup or constraints between actions.
