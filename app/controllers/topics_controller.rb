9bcd086f069bde2951e59fd1f2b0d69b9df75285
Dan Uluma <danuluma@gmail.com>
2018-08-14 23:16:48 +0300

    added markdown

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,4 +1,5 @@
 class TopicsController < ApplicationController
+  before_action :set_sidebar_topics
   layout 'blog'
   def index
     @topics = Topic.all
@@ -16,3 +17,12 @@ class TopicsController < ApplicationController
 
     end
   end
+
+  private
+
+  def set_sidebar_topics
+    @set_sidebar_topics = Topic.with_blogs
+    
+  end
+
+end
--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -1,5 +1,25 @@
 module BlogsHelper
   def gravatar_helper user
-    image_tag "https://web.whatsapp.com/", width: 60
+    image_tag "https://www.gravatar.com/", width: 60
+  end
+
+  class CodeRayify < RedCarpet::Render::HTML
+    def block_code(code, language)
+      CodeRay.scan(code, language) 
+    end
+  end
+
+  def markdown(text)
+    coderayfied = CodeRayify.new(filter_html: true, hard_wrap: true)
+
+    options = {
+      fenced_codeblocks: true,
+      no_intra_emphasis: true,
+      autolinks: true,
+      lax_html_blocks: true,
+    }
+
+    markdown_to_html = RedCarpet::Markdown.new(coderayfied, options)
+    markdown_to_html.render(text).html_safe
   end
 end
